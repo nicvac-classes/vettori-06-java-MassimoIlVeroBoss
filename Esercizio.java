cd2ac888365d1723701204c914474d93e0af2564
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2024-11-28 17:29:41 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,85 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        String[] v = new String[5];
+        String[] w = new String[5];
+        int[] x = new int[10];
+        int[] y = new int[10];
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        v[0] = "Piero";
+        v[1] = "Marco";
+        v[2] = "Francesca";
+        v[3] = "Anna";
+        v[4] = "Luigi";
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        i = 0;
+        while (i < 5) {
+            System.out.println("Inserire nome del " + (i + 1) + " cantante");
+            w[i] = scanner.nextLine();
+            i = i + 1;
+        }
+
+        i = 2;
+        while (i <= 10) {
+            System.out.println(i);
+            x[i] = i;
+            i = i + 2;
+        }
+
+        i = 0;
+        while (i < 10) {
+            x[i] = i;
+        }
+
+        System.out.println(v[0]);
+        System.out.println(v[1]);
+        System.out.println(v[2]);
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(v[i]);
+            i = i + 1;
+        }
+
+        i = 0;
+        while (i < 5) {
+            System.out.println(w[i]);
+            i = i + 1;
+        }
+
+        i = 0;
+        while (i < 10) {
+            System.out.println(x[i]);
+            i = i + 1;
+        }
+
+        i = 0;
+        while (i < 10) {
+            System.out.println(y[i]);
+            i = i + 1;
+        }
+
+        i = 1;
+        while (i <= 10) {
+            System.out.println(x[i]);
+            i = i + 2;
+        }
+
+        i = 0;
+        while (i <= 10) {
+            System.out.println(x[i]);
+            i = i + 2;
+        }
+
+        i = 1;
+        while (i <= 10) {
+            System.out.println(x[i]);
+            i = i + 2;
+        }
+
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
